25295ee1f0b7c3d52c7cb4468f32871c27c7e66a
Bxk756 <225787637+Bxk756@users.noreply.github.com>
2025-11-20 23:33:07 -0500

    Update AdminRoutes.jsx

--- src/AdminRoutes.jsx
+++ src/AdminRoutes.jsx
@@ -4,6 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
 import LogsPage from "./pages/LogsPage";
 import BillingPage from "./pages/BillingPage";
+import BillingSuccess from "./pages/BillingSuccess";
+import BillingCancel from "./pages/BillingCancel";
 import SettingsPage from "./pages/SettingsPage";
 
 export default function AdminRoutes() {
@@ -13,6 +15,8 @@ export default function AdminRoutes() {
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/logs" element={<LogsPage />} />
       <Route path="/billing" element={<BillingPage />} />
+      <Route path="/billing/success" element={<BillingSuccess />} />
+      <Route path="/billing/cancel" element={<BillingCancel />} />
       <Route path="/settings" element={<SettingsPage />} />
     </Routes>
   );
