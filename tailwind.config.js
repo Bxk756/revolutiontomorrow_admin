737610dce5ee7b112651374473eb11633675ff11
Bxk756 <225787637+Bxk756@users.noreply.github.com>
2025-11-20 20:26:35 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        midnight: "#020617",
+        cyber: "#22d3ee",
+        violet: "#7c3aed",
+      },
+    },
+  },
+  plugins: [],
+};
