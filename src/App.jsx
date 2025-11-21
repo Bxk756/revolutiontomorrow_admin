bed8e9aed4b7bba6bf9d9a2815b20bc87afccf5e
Bxk756 <225787637+Bxk756@users.noreply.github.com>
2025-11-20 21:01:20 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import FeatureGrid from "./components/FeatureGrid";
+import IntegrationStrip from "./components/IntegrationStrip";
+import AnalyticsBlock from "./components/AnalyticsBlock";
 import APIDemo from "./components/APIDemo";
+import APICodeSample from "./components/APICodeSample";
+import CTASection from "./components/CTASection";
+import StatsPanel from "./components/StatsPanel";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
+      <Navbar />
       <Hero />
       <Features />
+      <FeatureGrid />
+      <IntegrationStrip />
+      <AnalyticsBlock />
+      <StatsPanel />
       <APIDemo />
+      <APICodeSample />
+      <CTASection />
       <Footer />
     </>
   );
