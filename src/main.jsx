a34c32091cb40ed90f580ba0601f7fdfede0ebda
Bxk756 <225787637+Bxk756@users.noreply.github.com>
2025-11-24 11:30:43 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+// ---------------------------
 // Mount React App
+// ---------------------------
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
@@ -25,4 +27,3 @@ if ("serviceWorker" in navigator) {
       });
   });
 }
-
